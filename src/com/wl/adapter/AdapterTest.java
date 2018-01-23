680f42331299b9aedc56deaf3fba3a528f1d6f81
汪磊 <justwanglei@foxmail.com>
2018-01-23 16:24:16 +0800

    适配器模式

---
+++ src/com/wl/adapter/AdapterTest.java
@@ -0,0 +1,21 @@
+/**
+ * add by wanglei at 2018-1-23 下午4:16:32
+ * 
+ */
+package com.wl.adapter;
+
+/**
+ * @author wanglei
+ * 2018-1-23 下午4:16:32
+ */
+public class AdapterTest {
+
+	/**
+	 * add by wanglei at 2018-1-23 下午4:16:32
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
